98ccc9384639316f75753f8bb23dcc70d50230b2
Trevor Bedford <trvrb@trevorbedford.com>
2015-07-18 17:40:50 -0700

    Update Wright-Fisher iPython notebook and exercise.

--- wright-fisher/README.md
+++ wright-fisher/README.md
@@ -11,13 +11,29 @@ There are *N* individuals in the population. Every generation, each individual h
 ### Wright-Fisher model with mutation and genetic drift
 
  * [iPython notebook](https://github.com/trvrb/sismid/blob/master/wright-fisher/mutation-genetic-drift.ipynb)
- * [Python script](https://github.com/trvrb/sismid/blob/master/wright-fisher/mutation-genetic-drift.py)
-
-Run script with:
-
-	python mutation-genetic-drift.py --pop_size 100 --mutation_rate 0.002 --generations 200
 
  * [Visualization of Wright-Fisher haplotype dynamics](http://bedford.io/projects/haplotypes/)
+
+### *Exercise: effects of population size and mutation rate on observed haplotype dynamics*
+
+We'll go into this further in the next section, but a very important parameter is *&theta;*, which is equal to 2<i>N&mu;</i>, where *N* is equal to the population size and *&mu;* is equal to mutations per generation. If there are 100 sites and a per-site per-gen mutation rate of 0.0001, then *&mu;* = 0.01. If there are 50 individuals in the population, then *&theta;* = 2<i>N&mu;</i> = 1.
+
+This exercise can be completed by running the supplied Python script [`mutation-genetic-drift.py`](https://github.com/trvrb/sismid/blob/master/wright-fisher/mutation-genetic-drift.py). To run the script with population size of 50, per-site per-gen mutation rate of 0.0001, 100 sites and 500 generations, input:
+
+```
+python mutation-genetic-drift.py --pop_size 50 --mutation_rate 0.0001 --seq_length 100 --generations 500
+```
+
+Again, these parameters give *&theta;* = 1.
+
+1. **Keeping the above parameters as baseline, adjust population size up and down to vary *&theta;* between ~0.2 and ~5. What happens to diversity, divergence and haplotype dynamics?**
+2. **Keeping the above parameters as baseline, adjust mutation rate up and down to vary *&theta;* between ~0.2 and ~5. Again, what happens to diversity, divergence and haplotype dynamics?**
+
+You might try increasing `generations` to something greater than 500 to get a better feel for equilibrium divergence and diversity, say `--generations 2500`. In this case, it may be easier to not plot the haplotype trajectories. This can be done with `--no_hap`, like so:
+
+```
+python mutation-genetic-drift.py --pop_size 50 --mutation_rate 0.0001 --seq_length 100 --generations 2500 --no_hap
+```
 
 ### Neutral expectations for population summary statistics
 
--- wright-fisher/mutation-genetic-drift.py
+++ wright-fisher/mutation-genetic-drift.py
@@ -5,11 +5,11 @@ import matplotlib as mpl
 import argparse
 
 # global variables
-pop_size = 100
-seq_length = 10
+pop_size = 50
+seq_length = 100
 alphabet = ['A', 'T', 'G', 'C']
-mutation_rate = 0.002 # per gen per individual per site
-generations = 200
+mutation_rate = 0.0001 # per gen per individual per site
+generations = 500
 
 # population
 base_haplotype = ''.join(["A" for i in range(seq_length)])
@@ -77,7 +77,62 @@ def simulate():
         clone_pop = dict(pop)
         history.append(clone_pop)
 
-# plotting
+# plot diversity
+def get_distance(seq_a, seq_b):
+    diffs = 0
+    length = len(seq_a)
+    assert len(seq_a) == len(seq_b)
+    for chr_a, chr_b in zip(seq_a, seq_b):
+        if chr_a != chr_b:
+            diffs += 1
+    return diffs / float(length)
+
+def get_diversity(population):
+    haplotypes = population.keys()
+    haplotype_count = len(haplotypes)
+    diversity = 0
+    for i in range(haplotype_count):
+        for j in range(haplotype_count):
+            haplotype_a = haplotypes[i]
+            haplotype_b = haplotypes[j]
+            frequency_a = population[haplotype_a] / float(pop_size)
+            frequency_b = population[haplotype_b] / float(pop_size)
+            frequency_pair = frequency_a * frequency_b
+            diversity += frequency_pair * get_distance(haplotype_a, haplotype_b)
+    return diversity
+
+def get_diversity_trajectory():
+    trajectory = [get_diversity(generation) for generation in history]
+    return trajectory
+
+def diversity_plot(xlabel="generation"):
+    mpl.rcParams['font.size']=14
+    trajectory = get_diversity_trajectory()
+    plt.plot(trajectory)    
+    plt.ylabel("diversity")
+    plt.xlabel(xlabel)
+    
+# plot divergence
+def get_divergence(population):
+    haplotypes = population.keys()
+    divergence = 0
+    for haplotype in haplotypes:
+        frequency = population[haplotype] / float(pop_size)
+        divergence += frequency * get_distance(base_haplotype, haplotype)
+    return divergence
+
+def get_divergence_trajectory():
+    trajectory = [get_divergence(generation) for generation in history]
+    return trajectory
+
+def divergence_plot(xlabel="generation"):
+    mpl.rcParams['font.size']=14
+    trajectory = get_divergence_trajectory()
+    plt.plot(trajectory)
+    plt.ylabel("divergence")
+    plt.xlabel(xlabel) 
+
+# plot trajectories
 def get_frequency(haplotype, generation):
     pop_at_generation = history[generation]
     if haplotype in pop_at_generation:
@@ -99,28 +154,40 @@ def get_all_haplotypes():
 def stacked_trajectory_plot():
 	colors = ["#781C86", "#571EA2", "#462EB9", "#3F47C9", "#3F63CF", "#447CCD", "#4C90C0", "#56A0AE", "#63AC9A", "#72B485", "#83BA70", "#96BD60", "#AABD52", "#BDBB48", "#CEB541", "#DCAB3C", "#E49938", "#E68133", "#E4632E", "#DF4327", "#DB2122"]
 	mpl.rcParams['font.size']=18
-	plt.figure(num=None, figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
 	haplotypes = get_all_haplotypes()
 	trajectories = [get_trajectory(haplotype) for haplotype in haplotypes]
 	plt.stackplot(range(generations), trajectories, colors=colors)
 	plt.ylim(0, 1)
 	plt.ylabel("frequency")
 	plt.xlabel("generation")
-	plt.show()
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description = "run wright-fisher simulation with mutation and genetic drift")
-	parser.add_argument('--pop_size', type = int, default = 100.0, help = "population size")
-	parser.add_argument('--mutation_rate', type = float, default = 0.002, help = "mutation rate")
-	parser.add_argument('--seq_length', type = int, default = 10, help = "sequence length")
-	parser.add_argument('--generations', type = int, default = 200, help = "generations")	
-	
-	params = parser.parse_args()	
+	parser.add_argument('--pop_size', type = int, default = 50.0, help = "population size")
+	parser.add_argument('--mutation_rate', type = float, default = 0.0001, help = "mutation rate")
+	parser.add_argument('--seq_length', type = int, default = 100, help = "sequence length")
+	parser.add_argument('--generations', type = int, default = 500, help = "generations")
+	parser.add_argument('--no_hap', action = "store_true", default = False, help = "don't plot haplotypes")		
+
+	params = parser.parse_args()
 	pop_size = params.pop_size
 	mutation_rate = params.mutation_rate
 	seq_length = params.seq_length
 	generations = params.generations		
 	
 	simulate()
-	stacked_trajectory_plot()
-	
+
+	plt.figure(num=None, figsize=(14, 10), dpi=80, facecolor='w', edgecolor='k')
+	if params.no_hap:
+		plt.subplot2grid((2,1), (0,0))
+		diversity_plot()
+		plt.subplot2grid((2,1), (1,0))
+		divergence_plot()
+	else:	
+		plt.subplot2grid((2,2), (0,0), colspan=2)
+		stacked_trajectory_plot()
+		plt.subplot2grid((2,2), (1,0))
+		diversity_plot()
+		plt.subplot2grid((2,2), (1,1))
+		divergence_plot()
+	plt.show()		
